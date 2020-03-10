80442203af23362b080a4781c70e9f22dcedb13e
Severin Spoerri <severin.spoerri@outlook.com>
2020-03-10 01:01:45 +0100

    added comments

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -18,14 +18,14 @@ export class AuthGuard implements CanActivate {
       route: ActivatedRouteSnapshot,
       state: RouterStateSnapshot
    ): boolean | UrlTree | Promise<boolean | UrlTree> | Observable<boolean | UrlTree> {
-      // this one is tough. We need to return the login state, so if there is am authenticated user or not.
+      // this one is tough. We need to return the login state, so if there is an authenticated user or not.
       // We therefore subscribe to the BehaviourSubject that returns a user object and not a boolean.
       // We fix that with a pipe and the map operator and the !! operator in the end.
       // it converts the object to ture or false.
 
       // by adding the new URL Tree we can also redirect the user form whithin the auth guard.
 
-      // take one has to be here because we only whant the value once! otherwise this can lead to sideeffects.
+      // take one has to be here because we only whant the value once! otherwise this can lead to side effects.
       return this.authService.user.pipe(
          take(1),
          map(user => {
@@ -33,6 +33,7 @@ export class AuthGuard implements CanActivate {
             if (isUserAuth) {
                return true;
             }
+            // this allows rerouting from whithin the guard. this is a relatively new feature.
             return this.router.createUrlTree(["/auth"]);
          })
       );
